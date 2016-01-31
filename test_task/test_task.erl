b189d2511123424aa6d10c069e70814037f59fc2
Konstantin Chugalinskiy <kchugalinskiy@yandex.ru>
2016-01-31 23:27:23 +0300

    Get request from arguments

--- test_task/test_task.erl
+++ test_task/test_task.erl
@@ -1,19 +1,23 @@
 -module(test_task).
 -export([out/1]).
 
-
--include("..\\include\\yaws_api.hrl").
+-include_lib("eunit/include/eunit.hrl").
+-include("../include/yaws_api.hrl").
 
 box(Str) ->
     {'div',[{class,"box"}],
      {pre,[],Str}}.
 
 out(A) ->
+    Request = A#arg.req,
+    RequestMethod = Request#http_request.method,
     {ehtml,
      [{p,[],
-       box(io_lib:format("A#arg.appmoddata = ~p~n"
-                         "A#arg.appmod_prepath = ~p~n"
-                         "A#arg.querydata = ~p~n",
-                         [A#arg.appmoddata,
-                          A#arg.appmod_prepath,
-                          A#arg.querydata]))}]}.
+       box(io_lib:format("A#arg.pathinfo = ~p~n"
+                         "RequestMethod = ~p~n"
+                         "Request = ~p~n"
+                         "A = ~p~n",
+                         [A#arg.pathinfo,
+                          RequestMethod,
+                          Request,
+                          A]))}]}.
