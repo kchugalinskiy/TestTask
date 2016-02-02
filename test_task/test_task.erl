96e654ad3b4bb64638dab7a75404c377d199cae5
Konstantin Chugalinskiy <kchugalinskiy@yandex.ru>
2016-02-03 00:32:20 +0300

    sample json, rfc4627

--- test_task/test_task.erl
+++ test_task/test_task.erl
@@ -7,30 +7,25 @@
 start() ->
 	resource_server:start_link().
 
-box(Str) ->
-    {'div',[{class,"box"}],
-     {pre,[],Str}}.
-
 out(A) ->
-    Reply = resource_server:allocate("Alice"),
     Request = A#arg.req,
-    OldState = A#arg.state,
-    NewArg = A#arg{ state = 1 },
     RequestMethod = Request#http_request.method,
-    %http://stackoverflow.com/questions/7864784/how-to-maintain-stateful-in-yaws
-    {ehtml,
-     [{p,[],
-       box(io_lib:format("Reply = ~p~n"
-                         "A#arg.pathinfo = ~p~n"
-                         "RequestMethod = ~p~n"
-                         "Request = ~p~n"
-                         "State = ~p~n"
-                         "OldState = ~p~n"
-                         "A = ~p~n",
-                         [Reply,
-                          A#arg.pathinfo,
-                          RequestMethod,
-                          Request,
-                          A#arg.state,
-                          OldState,
-                          A]))}]}.
+    dispatch_request(A#arg.pathinfo, RequestMethod, []),
+    JsonData = rfc4627:encode( {obj, [{"a", 1}, {"b", 2}]} ),
+    io:format("Json data : ~p~n", [JsonData]),
+    { html, JsonData }.
+
+dispatch_request("allocate", 'GET', [Resource]) ->
+  { ok, resource_server:allocate(Resource) };
+
+dispatch_request("deallocate", 'GET', [ResourceID]) ->
+  { ok, resource_server:deallocate(ResourceID) };
+
+dispatch_request("reset", 'GET', []) ->
+  { ok, resource_server:reset() };
+
+dispatch_request("list", 'GET', []) ->
+  { ok, resource_server:list() };
+
+dispatch_request(BadRequest, BadHTTPMethod, Params) ->
+  { error, { bad_request } }.
